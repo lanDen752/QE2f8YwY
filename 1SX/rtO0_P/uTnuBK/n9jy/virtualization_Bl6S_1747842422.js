02657a6d635196083bcade9f5636e53d5ceea60c
lanDen752 <biker_sorbets_0l@icloud.com>
2025-05-23 04:04:52 +0100

    Fix failing tests in the file

--- 1SX/rtO0_P/uTnuBK/n9jy/virtualization_Bl6S_1747842422.js
+++ 1SX/rtO0_P/uTnuBK/n9jy/virtualization_Bl6S_1747842422.js
@@ -45,7 +45,6 @@ if (env === 'rollup') {
 
 if (env === 'development') {
   module.exports = {
-    plugins: ['@babel/plugin-proposal-class-properties'],
     presets: ['@babel/preset-react', '@babel/preset-flow'],
   };
 }
